b27d880a6beeebc3aa9ecd7a032be7a4b68e9a60
Alex Knowlton <alexrules313@gmail.com>
2019-04-14 17:58:30 -0400

    edited model

--- Hash.py
+++ Hash.py
@@ -83,7 +83,7 @@ class DoubleDense(nn.Module):
         self.fc3 = nn.Linear(512, 256)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
         x = self.fc3(x)
         return torch.round(torch.sigmoid(x))
